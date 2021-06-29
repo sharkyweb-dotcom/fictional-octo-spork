4fcecc340255a891c49d349908da9f670a6ca5fd
Finn Archer <finnharcher@gmail.com>
2021-06-28 20:21:16 -0500

    Added mouse tracking

--- ratings/ratings.js
+++ ratings/ratings.js
@@ -21,3 +21,31 @@ function filter() {
     console.log(filteredArray)
     document.getElementById('inputwhy').value=filteredArray.join(' ');
 }
+const listenedList=['range','inputwhy', 'inputsuggest']
+let timeOver;
+let o;
+let timeOut;
+let t;
+let inputArray=['I have done these things:'];
+listenedList.forEach(item=>{
+    document.getElementById(item).addEventListener("mouseover", function() {
+        inputArray.push(`Mouseover: ${item}`);
+        o=new Date;
+        timeOver=o.getTime();
+        document.getElementById('inputTracked').value=inputArray.join('\n');
+        console.log(document.getElementById('inputTracked').value);
+    });
+    document.getElementById(item).addEventListener("mouseout", function() {
+        t=new Date;
+        timeOut=t.getTime();
+        console.log('Seconds hovering over '+item+(timeOut-timeOver)/1000);
+        inputArray.push(`Mouseout: ${item} \nSeconds hovering over ${item}:`);
+        inputArray.push((timeOut-timeOver)/1000);
+        document.getElementById('inputTracked').value=inputArray.join('\n');
+    });
+    document.getElementById(item).addEventListener("click", function() {
+        console.log(`Clicked: ${item}`)
+        inputArray.push(`Clicked: ${item}`)
+        document.getElementById('inputTracked').value=inputArray.join('\n');
+    });
+})
